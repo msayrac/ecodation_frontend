a905218910955cf535830eeaa13ee8341729c20e
msayrac <msayracode@gmail.com>
2023-07-02 14:50:25 +0300

    Search,  Dark Mode, Header, Footer

--- template.js
+++ template.js
@@ -1,5 +1,38 @@
 
-// alert("Deneme")
+// Dark Mode
+let darkMode = () => {
+    //alert("Dark Mode");
+    window.document.classList.toggle("dark_mode")
 
-// window.alert("Deneme")
+
+}
+
+//input search
+$(document).ready(function () {
+    const searchApi = ["Adana", "Balıkesir", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Malatya", "Van"]
+    $("#tags").autocomplete({
+        source: searchApi
+    })
+
+}); // end
+
+
+
+
+
+//Footer
+let newDate = () => {
+    //JS DOM
+    // document.getElementById("date_id").innerHTML= new Date().getFullYear();
+
+
+    //jquery DOM nasıl çalışır araştır
+    const date = new Date().getFullYear();
+    $("#date_id").html(date)
+}
+
+// Function called.
+newDate();
+
+
 
