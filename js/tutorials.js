5ae775dd151f351894ed980d0b4750cf5df70af8
msayrac <msayracode@gmail.com>
2023-07-09 13:38:07 +0300

    whoisting, variable

--- js/tutorials.js
+++ js/tutorials.js
@@ -4,16 +4,40 @@
 multiple commnet
 */
 
-console.log("loglama");
-console.warn("warn");
-console.error("error");
-console.info("infor"); 
-alert("pop-up bilgilendirme 1 ")
+// console.log("loglama");
+// console.warn("warn");
+// console.error("error");
+// console.info("infor"); 
+// alert("pop-up bilgilendirme 1 ")
 
-window.alert("pop-up bilgilendirme 2");
+// window.alert("pop-up bilgilendirme 2");
 
-window.document.writeln("Ekranda yazı");
+// window.document.writeln("Ekranda yazı");
 
+// variable
+
+var result = "Js ögreniyorum";
+console.log(typeof result + " => " + result);
+
+var result = 14.53;
+console.log(typeof result + " => " + result);
+
+var result = true;
+console.log(typeof result + " => " + result);
+
+//Naming convention
+
+var $deneme44 ="JS Data";
+
+console.log($deneme44)
+
+result2 = 56;
+
+var result2;
+
+
+////////////////////////////////
+// operators
 
 
 
