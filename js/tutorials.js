e3824a4e696dceb393635b0cecda0228968b2a1f
msayrac <msayracode@gmail.com>
2023-07-09 13:20:48 +0300

    javascript

--- js/tutorials.js
+++ js/tutorials.js
@@ -4,6 +4,16 @@
 multiple commnet
 */
 
+console.log("loglama");
+console.warn("warn");
+console.error("error");
+console.info("infor"); 
+alert("pop-up bilgilendirme 1 ")
+
+window.alert("pop-up bilgilendirme 2");
+
+window.document.writeln("Ekranda yazı");
+
 
 
 
