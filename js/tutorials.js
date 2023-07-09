e0440f3663b7edf1e83445c348dd99bfb6efe75e
msayrac <msayracode@gmail.com>
2023-07-09 13:52:36 +0300

    Math Undifined

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,43 +1,67 @@
 
-// single commet
-/*
-multiple commnet
-*/
+// // single commet
+// /*
+// multiple commnet
+// */
 
-// console.log("loglama");
-// console.warn("warn");
-// console.error("error");
-// console.info("infor"); 
-// alert("pop-up bilgilendirme 1 ")
+// // console.log("loglama");
+// // console.warn("warn");
+// // console.error("error");
+// // console.info("infor"); 
+// // alert("pop-up bilgilendirme 1 ")
 
-// window.alert("pop-up bilgilendirme 2");
+// // window.alert("pop-up bilgilendirme 2");
 
-// window.document.writeln("Ekranda yazı");
+// // window.document.writeln("Ekranda yazı");
 
-// variable
+// // variable
 
-var result = "Js ögreniyorum";
-console.log(typeof result + " => " + result);
+// var result = "Js ögreniyorum";
+// console.log(typeof result + " => " + result);
 
-var result = 14.53;
-console.log(typeof result + " => " + result);
+// var result = 14.53;
+// console.log(typeof result + " => " + result);
 
-var result = true;
-console.log(typeof result + " => " + result);
+// var result = true;
+// console.log(typeof result + " => " + result);
 
-//Naming convention
+// //Naming convention
 
-var $deneme44 ="JS Data";
+// var $deneme44 ="JS Data";
 
-console.log($deneme44)
+// console.log($deneme44)
 
-result2 = 56;
+// result2 = 56;
 
-var result2;
+// var result2;
 
 
-////////////////////////////////
-// operators
+// ////////////////////////////////
+// // operators
+
+// // + - * /  **  %
+// // x++ x--  ++x --x  = == === != &&  ||
+
+// var userData = Number(prompt("Lutfen sayı giriniz : "));
+
+// console.log(userData+45);
+
+// undifined
+//nan
+//isNaN
+//Infinity
+
+var result = "add"/4
+console.log(result);
+
+console.log(Math.PI)
+
+
+
+
+
+
+
 
 
 
