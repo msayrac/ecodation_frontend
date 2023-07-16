e6f0ba3ceae7d39a1febbe17f45d941e63529f8b
msayrac <msayracode@gmail.com>
2023-07-16 15:29:24 +0300

    for, for in, for of LOOPS

--- js/tutorials.js
+++ js/tutorials.js
@@ -44,24 +44,66 @@
 // ikinci(birinci);
 
 
-const promTutorials = () => {
+// const promTutorials = () => {
 
-    // catch 1 tane olmak zorunda
-    let data = new Promise((resolve, reject) => { 
-        let status = 400;
+//     // catch 1 tane olmak zorunda
+//     let data = new Promise((resolve, reject) => { 
+//         let status = 400;
 
-        if(status===200){
-            resolve("Çalıştı");
-        } else
-        reject("çalışmadı")
-    }).then(
-            () => { console.log("OLUMLU"); }
-        ).catch(
-            (err) => {console.error(err);}
-        );
+//         if(status===200){
+//             resolve("Çalıştı");
+//         } else
+//         reject("çalışmadı")
+//     }).then(
+//             () => { console.log("OLUMLU"); }
+//         ).catch(
+//             (err) => {console.error(err);}
+//         );
+// };
+
+// promTutorials();
+
+
+// arrays
+
+let arr = () => {
+    const numbers = [1, 3, 5, "str", true,66,99,852,65];
+    return numbers;
+
 };
 
-promTutorials();
+arr()
+
+
+let arrResult = () => {
+    let data = arr();
+    console.log(data);
+    console.log(data[data.length - 1]);
+    //Iterative for loop
+
+    for (let i = 0; i < data.length; i++) {
+        document.write(data[i] + " ");
+        
+    }
+    document.write("<br/>+++++++++++++++++++++++ <br/>")
+
+    for(let temp in data){
+        document.writeln(data[temp] + " ");
+    }
+    document.write("<br/>+++++++++++++++++++++++ <br/>")
+    for(let temp of data){
+        document.write(temp + " ")
+    }
+
+};
+
+//for in
+
+
+
+arrResult();
+
+
 
 
 
